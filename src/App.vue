f1bf7db7c8137fb68d878a2eef231b09ed05b484
Kaz <takaik@yahoo.com>
2022-04-08 14:03:11 -0400

    add authentication

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Home</router-link>|
+    <router-link to="/about">About</router-link>|
+    <router-link to="/posts">Posts</router-link>|
+    <router-link to="/signup">Signup</router-link>|
+    <router-link to="/login">Login</router-link>|
+    <router-link to="/logout">Logout</router-link>
   </nav>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
