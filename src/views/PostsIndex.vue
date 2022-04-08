8abb398555c6b2067a1338449b6a89c77dfb7e20
Kaz <takaik@yahoo.com>
2022-04-08 14:30:24 -0400

    add indexPosts method

---
+++ src/views/PostsIndex.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+    <div v-for="post in posts" :key="post.id">
+      <hr />
+      <hr />
+      <p>ID: {{ post.id }}</p>
+      <h3>{{ post.title }}</h3>
+      <p>{{ post.body }}</p>
+      <img class="post-img" :src="post.image" />
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios"
+export default {
+  data() {
+    return {
+      posts: [],
+    }
+  },
+  created() {
+    this.indexPosts();
+  },
+  methods: {
+    indexPosts() {
+      axios.get("posts")
+        .then((res) => {
+          this.posts = res.data
+        })
+    },
+  },
+}
+</script>
+
+<style>
+.post-img {
+  max-height: 200px;
+}
+</style>
